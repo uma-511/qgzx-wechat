25e1ae5fd37a75814be21736047f07642257394b
yangguifang <yangguifang@192.168.0.101>
2017-10-31 12:00:00 +0800

    我的应聘代码提交

--- weixin/src/components/grzx/Wdyp.vue
+++ weixin/src/components/grzx/Wdyp.vue
@@ -1,27 +1,23 @@
 <template>
   <div class="contentDiv">
    <search
-    @result-click="resultClick"
-    @on-change="getResult"
-    :results="results"
-    v-model="value"
+       v-model="keyword"
+    :auto-fixed="autoFixed"
     position="absolute"
     placeholder="可输入岗位名称或工作单位"
-    auto-scroll-to-top top="46px"
-    @on-focus="onFocus"
     @on-cancel="onCancel"
     @on-submit="onSubmit"
     ref="search"></search>
      <tab :line-width="2" custom-bar-width="100px" >
-      <tab-item selected >固定岗</tab-item>
-      <tab-item>临时岗</tab-item>
+      <tab-item selected @on-item-click="handler('固定岗')">固定岗</tab-item>
+      <tab-item @on-item-click="handler('临时岗')">临时岗</tab-item>
     </tab>
     <group gutter="0">
       
-      <cell is-link title="岗位名称"  v-for="item in lists" @click.native="onClickDetail(item.id)" inline-desc='公司名称' style="border-top:0px;">
-       <span slot="title" ><span style="vertical-align:middle;">{{item.gwmc}}</span>&nbsp;<img src="static/hot1.png" width="20" height="20" v-if="item.sfrm==1"/></span>
-       <div slot="inline-desc"><div style="font-size:10px;padding-bottom:10px;">{{item.gzdd}}</div><div style="font-size:14px;color:black;">{{item.ggmc}}<br/><badge style="background-color:#FF7951;" text="申请中" v-if="item.sfyp==1"></badge><badge style="background-color:#09BB07;" text="成功应聘" v-if="item.sfyp==2"></badge></div></div>
-       <div slot="default" style="padding-right:10px;"><div style="font-size:14px;padding-bottom:5px;color:black;">8千-1万</div><div style="font-size:14px;">{{item.fbsj}}</div></div>
+      <cell is-link title="岗位名称"  v-for="item in lists" @click.native="onClickDetail(item.job_id)" inline-desc='公司名称' style="border-top:0px;">
+       <span slot="title" ><span style="vertical-align:middle;">{{item.post_name}}</span></span>
+       <div slot="inline-desc"><div style="font-size:10px;padding-bottom:10px;">{{item.address}}</div><div style="font-size:14px;color:black;">{{item.unit_name}}<br/><badge style="background-color:#FF7951;" v-bind:text="item.status_list" v-if="item.status_list=='待审核'"></badge><badge style="background-color:#09BB07;" v-bind:text="item.status_list" v-if="item.status_list=='已录取'"></badge></div></div>
+       <div slot="default" style="padding-right:10px;"><div style="font-size:14px;padding-bottom:5px;color:black;">面议</div><div style="font-size:14px;">{{item.release_time}}</div></div>
       </cell>
     </group>
 <divider>到达底部了</divider>
@@ -48,71 +44,44 @@ export default {
   created: function () {
     // this.closeShowBack()
     this.updateTitle('我的应聘')
-    // this.getData()
+    this.getData()
   },
   methods: {
-    setFocus () {
-      this.$refs.search.setFocus()
-    },
-    resultClick (item) {
-      window.alert('you click the result item: ' + JSON.stringify(item))
-    },
-    getResult (val) {
-      this.results = val ? getResult(this.value) : []
+    onCancel () {
+      if (this.keyword !== '') {
+        this.keyword = ''
+        this.getData()
+      }
     },
     onSubmit () {
-      this.$refs.search.setBlur()
-      this.$vux.toast.show({
-        type: 'text',
-        position: 'top',
-        text: 'on submit'
-      })
+      this.getData()
     },
-    onFocus () {
-      console.log('on focus')
-    },
-    onCancel () {
-      console.log('on cancel')
+    handler (bz) {
+      this.quality = bz
+      this.getData()
     },
     onClickDetail (id) {
-      this.$router.push({path: '/components/zpzx/Gwxq', query: {job_id: 13, statue: 1}})
+      this.$router.push({path: '/components/zpzx/Gwxq', query: {job_id: id, statue: 1}})
     },
     getData () {
       let vue = this
       vue.post({
-        url: '/dishui/zjsxhQuery',
-        params: {'cxbj': '1', 'paraval': '913305005633351465', 'paramc': 'nsrsbh'},
+        url: '/public/api/person/getApply',
+        params: {'id': vue.GLOBAL.student.id, 'keyword': vue.keyword, 'quality': vue.quality},
         success: function (data) {
-          let data1 = [{'gwmc': 'java程序员', 'gzdd': '佛山市-南海区', 'ggmc': '佛山阿啪啪信息科技有限公司', 'cx': '8千-1万', 'fbsj': '2017-09-09', 'gwxz': '1', 'sfrm': '1', 'gzxxdd': '', 'zwms': '', sfyp: '1'},
-            {'gwmc': '美工', 'gzdd': '佛山市-南海区', 'ggmc': '佛山阿啪啪信息科技有限公司', 'cx': '8千-1万', 'fbsj': '2017-09-09', 'gwxz': '2', 'sfrm': '0', 'gzxxdd': '', 'zwms': '', sfyp: '2'},
-            {'gwmc': '需求工程师', 'gzdd': '佛山市-南海区', 'ggmc': '佛山阿啪啪信息科技有限公司', 'cx': '8千-1万', 'fbsj': '2017-09-09', 'gwxz': '1', 'sfrm': '0', 'gzxxdd': '', 'zwms': '', sfyp: '1'},
-            {'gwmc': '项目经理', 'gzdd': '佛山市-南海区', 'ggmc': '佛山阿啪啪信息科技有限公司', 'cx': '8千-1万', 'fbsj': '2017-09-09', 'gwxz': '1', 'sfrm': '0', 'gzxxdd': '', 'zwms': '', sfyp: '0'}]
-          vue.lists = data1
+          vue.lists = data.list
         }
       })
     }
   },
   data () {
     return {
-      results: [],
-      value: '',
-      lists: [{'gwmc': 'java程序员', 'gzdd': '佛山市-南海区', 'ggmc': '佛山阿啪啪信息科技有限公司', 'cx': '8千-1万', 'fbsj': '2017-09-09', 'gwxz': '1', 'sfrm': '1', 'gzxxdd': '', 'zwms': '', sfyp: '1'},
-            {'gwmc': '美工', 'gzdd': '佛山市-南海区', 'ggmc': '佛山阿啪啪信息科技有限公司', 'cx': '8千-1万', 'fbsj': '2017-09-09', 'gwxz': '2', 'sfrm': '0', 'gzxxdd': '', 'zwms': '', sfyp: '2'},
-            {'gwmc': '需求工程师', 'gzdd': '佛山市-南海区', 'ggmc': '佛山阿啪啪信息科技有限公司', 'cx': '8千-1万', 'fbsj': '2017-09-09', 'gwxz': '1', 'sfrm': '0', 'gzxxdd': '', 'zwms': '', sfyp: '1'},
-            {'gwmc': '项目经理', 'gzdd': '佛山市-南海区', 'ggmc': '佛山阿啪啪信息科技有限公司', 'cx': '8千-1万', 'fbsj': '2017-09-09', 'gwxz': '1', 'sfrm': '0', 'gzxxdd': '', 'zwms': '', sfyp: '0'}]
+      lists: [],
+      keyword: '',
+      quality: '固定岗',
+      autoFixed: false
     }
   }
-}
-
-function getResult (val) {
-  let rs = []
-  for (let i = 0; i < 20; i++) {
-    rs.push({
-      title: `${val} result: ${i + 1} `,
-      other: i
-    })
-  }
-  return rs
 }
 
 </script>
